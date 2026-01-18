4382c0e782ae2e479876ed25640d2d7d7e4357da
Yuwei Li <snowtime200801@gmail.com>
2026-01-18 17:48:10 +0100

    update seo

---
+++ components/seo/json-ld.tsx
@@ -0,0 +1,37 @@
+// JSON-LD 结构化数据组件：帮助搜索引擎理解页面内容
+export function JsonLd({ data }: { data: Record<string, any> }) {
+    return (
+        <script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(data) }}
+        />
+    )
+}
+
+// 个人/组织结构化数据
+export const personSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'Person',
+    name: 'Yuwei Li',
+    url: 'https://yuweili.com',
+    sameAs: [
+        'https://github.com/yuweili',
+        'https://linkedin.com/in/yuweili',
+        'https://instagram.com/yuweili',
+    ],
+    jobTitle: 'Designer & Developer',
+    description: 'Designer, Developer, and Creative Technologist',
+}
+
+// 网站结构化数据
+export const websiteSchema = {
+    '@context': 'https://schema.org',
+    '@type': 'WebSite',
+    name: 'Yuwei Li Portfolio',
+    url: 'https://yuweili.com',
+    description: 'Portfolio and background of Yuwei Li - Designer, Developer, and Creative Technologist.',
+    author: {
+        '@type': 'Person',
+        name: 'Yuwei Li',
+    },
+}
